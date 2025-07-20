c2a96fd70b652cda8c55b00336b90a117d0b010e
ADN HacKeR <101885534+ADN127@users.noreply.github.com>
2025-07-20 18:49:59 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -574,7 +574,6 @@ def admin_page():
         <title>Admin Panel - PDF Yuklash</title>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <script src="/static/captcha-overlay.js"></script>
         <style>
             * {
                 margin: 0;
