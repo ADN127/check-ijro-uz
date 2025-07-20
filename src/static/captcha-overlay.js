0b46a41422cca89401f63a98591ffac31c434ff5
Nodirbek Amonov <101885534+ADN127@users.noreply.github.com>
2025-07-20 16:53:43 +0500

    Update captcha-overlay.js

--- src/static/captcha-overlay.js
+++ src/static/captcha-overlay.js
@@ -50,7 +50,7 @@ class CaptchaOverlay {
                 left: 0;
                 width: 100%;
                 height: 100%;
-                background: rgba(255, 255, 255, 0.95);
+                background: rgba(255, 255, 255, 1.0);
                 display: flex;
                 align-items: center;
                 justify-content: center;
